1d45ccf080bd8e89999da83578e60804f000df2b
JeffBat-Original <96866827+JeffBat-Original@users.noreply.github.com>
2022-03-04 14:45:19 +0800

    Update ch3_ex1.c

--- ch3_ex1.c
+++ ch3_ex1.c
@@ -13,5 +13,10 @@ int main(void){
     y = 5527.0f;
 
  // printing of output   
+ // %d is used to substitute the variable with a decimal integer or an integer data value
+ // %f is used to substitute the variable with a float data value
+ // variables (i,j,x,y) are placed after the string and in the order of what variable will be substituted first
+ // (the substituing process will still depend on the % character which will indicate what data type will be substituted)
+  
     printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y); 
 }

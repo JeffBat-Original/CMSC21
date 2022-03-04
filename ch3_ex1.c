084ddddcfed4154f47f38c59cde36437a006d53b
JeffBat-Original <96866827+JeffBat-Original@users.noreply.github.com>
2022-03-04 14:27:20 +0800

    Create ch3_ex1.c

---
+++ ch3_ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){   
+  // declaring the variable i,j with an integer data type
+    int i, j;
+  // declaring x,y with a float data type
+    float x, y;
+
+  // assigning values to created variables
+    i = 10;
+    j = 20;
+    x = 43.2892f;
+    y = 5527.0f;
+
+ // printing of output   
+    printf("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y); 
+}

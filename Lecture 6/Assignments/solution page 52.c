126d35c52044839610f6fecf499696de5d3c5a6a
JeffBat-Original <batogjeffrouzel@gmail.com>
2022-04-29 20:56:43 +0800

    commit

---
+++ Lecture 6/Assignments/solution page 52.c
@@ -0,0 +1,26 @@
+/* finds the negative numbers in an array */
+
+#include <studio.h>
+
+int main(void){
+    int N, i;
+
+    printf ("Enter N: ", N);
+    scanf ("%d", &N);
+
+    int a[N];
+
+    for (i = 0; i < N; i++){
+        printf("a[%d] = ", i);
+        scanf ("%d", &a[i]);
+    }
+
+    printf ("Negative numbers: \n");
+
+    for (i = 0; i < N; i++){
+        if (a[i] < 0){
+            printf ("a[%d] = %d is a negative. \n", i, a[i]);
+        }
+    }
+    return 0;
+}

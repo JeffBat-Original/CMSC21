01e42acf713b63f6b5db070afb2607efe39fe48e
JeffBat-Original <batogjeffrouzel@gmail.com>
2022-04-29 21:50:35 +0800

    commit

---
+++ Lecture 7/Assignments/Initialization.c
@@ -0,0 +1,19 @@
+int num[3][3]={{5,1,6},
+              {4,6,3}, 
+              {2,8,7}};
+
+
+
+int num[3][3]={{5,1,6}, {4,6,3}, {2,8,7}};
+
+
+/* valid initialization */
+
+int abc[2][2]= {1,2,3,4};
+int abc[][2]= {1,2,3,4};
+
+
+/* invalid initialization */
+
+int abc[][]= {1,2,3,4};
+int abc[2][]= {1,2,3,4};
